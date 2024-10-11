e8cecfca6d4c8dd1c64c0dd4a67fcb713bfce7cf
Sarai Rdz <angelicasarai51@gmail.com>
2024-10-11 00:54:14 -0600

    Se agrego un boton

---
+++ libros.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('Inicio').addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+
+    document.getElementById('Final').addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+});
